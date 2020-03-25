41e61eac908981c12a6db6503abc7cc97ba9f671
Miguel <llamamemiguel@hotmail.com>
2020-03-25 14:04:14 +0100

    pacstrap installation

---
+++ pacstrap.sh
@@ -0,0 +1,12 @@
+# pacstrap-ping desired disk
+pacstrap /mnt base base-devel vim grub networkmanager rofi feh linux linux-headers \
+os-prober efibootmgr ntfs-3g alacritty git zsh intel-ucode cpupower xf86-video-amdgpu vlc \
+xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
+chromium firefox code xf86-video-intel zip unzip unrar obs-studio docker \
+pulseaudio mate-media pamixer telegram-desktop go python python-pip wget \
+openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip pinta light ranger \
+ttf-roboto playerctl papirus-icon-theme hwloc p7zip picom hsetroot docker-compose \
+nemo linux-firmware firewalld tree man glances ttf-cascadia-code darktable fzf \
+mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver \
+mesa-vdpau lib32-mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr \
+xf86-video-nouveau xf86-video-vmware python-dbus httpie discord
