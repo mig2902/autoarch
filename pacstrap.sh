90c74df9d3d14750a8fe73a7df640f69f0f85ee8
Miguel <llamamemiguel@hotmail.com>
2020-03-25 18:29:27 +0100

    customizing conf

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,12 +1,13 @@
+# Driver para Ati radeon y kernel linux-zen
 # pacstrap-ping desired disk
-pacstrap /mnt base base-devel vim grub networkmanager rofi feh linux linux-headers \
-os-prober efibootmgr ntfs-3g alacritty git zsh intel-ucode cpupower xf86-video-amdgpu vlc \
+pacstrap /mnt base base-devel vim grub networkmanager rofi feh linux-zen linux-headers \
+ntfs-3g alacritty git zsh intel-ucode cpupower xf86-video-ati xf86-video-amdgpu vlc \
 xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
-chromium firefox code xf86-video-intel zip unzip unrar obs-studio docker \
+firefox code xf86-video-intel zip unzip unrar obs-studio docker \
 pulseaudio mate-media pamixer telegram-desktop go python python-pip wget \
-openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip pinta light ranger \
+openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip light ranger \
 ttf-roboto playerctl papirus-icon-theme hwloc p7zip picom hsetroot docker-compose \
-nemo linux-firmware firewalld tree man glances ttf-cascadia-code darktable fzf \
+nemo linux-firmware firewalld tree man glances ttf-cascadia-code fzf \
 mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver \
 mesa-vdpau lib32-mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr \
 xf86-video-nouveau xf86-video-vmware python-dbus httpie discord
--- pre-install.sh
+++ pre-install.sh
@@ -66,7 +66,21 @@ mount /dev/${selected_disk}1 /mnt/boot
 mount /dev/${selected_disk}2 /mnt/home
 
 echo ""
-echo -e "\e[1;42m>>> PRE-INSTALLATION COMPLETE...\e[0m"
+echo -e "\e[1;42mPRE-INSTALLATION COMPLETE...\e[0m"
 echo ""
 
 sleep 3
+
+# Driver para Ati radeon y kernel linux-zen
+# pacstrap-ping desired disk
+pacstrap /mnt base base-devel vim grub networkmanager rofi feh linux-zen linux-headers \
+ntfs-3g alacritty git zsh intel-ucode cpupower xf86-video-ati xf86-video-amdgpu vlc \
+xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
+firefox code xf86-video-intel zip unzip unrar obs-studio docker \
+pulseaudio mate-media pamixer telegram-desktop go python python-pip wget \
+openssh xorg-xrandr noto-fonts-emoji maim imagemagick xclip light ranger \
+ttf-roboto playerctl papirus-icon-theme hwloc p7zip picom hsetroot docker-compose \
+nemo linux-firmware firewalld tree man glances ttf-cascadia-code fzf \
+mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver \
+mesa-vdpau lib32-mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr \
+xf86-video-nouveau xf86-video-vmware python-dbus httpie discord
