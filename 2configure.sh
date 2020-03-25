797165e4bec0808c527576f9f616816bd47f9631
Miguel <llamamemiguel@hotmail.com>
2020-03-25 14:41:02 +0100

    2configure system script

--- 2configure.sh
+++ 2configure.sh
@@ -1,12 +1,12 @@
 # configuring miguel boot entry
 arch-chroot /mnt grep "UUID=" /etc/fstab | grep '/ ' | awk '{ print $1 }' | sed -e 's/UUID=//' > .root_disk_uuid
-arch-chroot /mnt touch /boot/loader/entries/miguel.conf
-arch-chroot /mnt echo "title miguel" >> /boot/loader/entries/miguel.conf
-arch-chroot /mnt echo "linux /vmlinuz-linux" >> /boot/loader/entries/miguel.conf
-arch-chroot /mnt echo "initrd /amd-ucode.img" >> /boot/loader/entries/miguel.conf
-arch-chroot /mnt echo "initrd /initramfs-linux.img" >> /boot/loader/entries/miguel.conf
-arch-chroot /mnt echo 'options root="UUID=root_disk_uuid" rw' >> /boot/loader/entries/miguel.conf
-arch-chroot /mnt sed -ie "s/root_disk_uuid/$(cat .root_disk_uuid)/g" /boot/loader/entries/miguel.conf
+arch-chroot /mnt touch /mnt/boot/loader/entries/miguel.conf
+arch-chroot /mnt echo "title miguel" >> /mnt/boot/loader/entries/miguel.conf
+arch-chroot /mnt echo "linux /vmlinuz-linux" >> /mnt/boot/loader/entries/miguel.conf
+arch-chroot /mnt echo "initrd /amd-ucode.img" >> /mnt/boot/loader/entries/miguel.conf
+arch-chroot /mnt echo "initrd /initramfs-linux.img" >> /mnt/boot/loader/entries/miguel.conf
+arch-chroot /mnt echo 'options root="UUID=root_disk_uuid" rw' >> /mnt/boot/loader/entries/miguel.conf
+arch-chroot /mnt sed -ie "s/root_disk_uuid/$(cat .root_disk_uuid)/g" /mnt/boot/loader/entries/miguel.conf
 arch-chroot /mnt rm .root_disk_uuid
 
 # changing governor to performance
