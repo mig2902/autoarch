a2cbae445af7548039131c541223ab1295583d1e
Miguel <llamamemiguel@hotmail.com>
2020-03-26 11:16:48 +0100

    add package list

--- configure.sh
+++ configure.sh
@@ -5,37 +5,9 @@ arch-chroot /mnt zsh -c 'echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacma
 # updating repo status
 arch-chroot /mnt pacman -Syyy
 
-# setting right timezone
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 
-# enabling font presets for better font rendering
-arch-chroot /mnt ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
-arch-chroot /mnt ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-arch-chroot /mnt ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
-# synchronizing timer
-arch-chroot /mnt hwclock --systohc
 
-# localizing system
-arch-chroot /mnt sed -ie 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/g' /etc/locale.gen
-arch-chroot /mnt sed -ie 's/#es_ES ISO-8859-1/es_ES ISO-8859-1/g' /etc/locale.gen
-arch-chroot /mnt sed -ie 's/#es_ES@euro ISO-8859-15/es_ES@euro ISO-8859-15/g' /etc/locale.gen
-
-# generating locale
-arch-chroot /mnt locale-gen
-
-# setting system language
-arch-chroot /mnt echo "LANG=es_ES.UTF-8" >> /mnt/etc/locale.conf
-
-# setting machine name
-arch-chroot /mnt echo "arch-laptop" >> /mnt/etc/hostname
-# Choose machine name
-#arch-chroot /mnt read -p "Choose your machine name (only one word):" machine_name
-
-# setting hosts file
-arch-chroot /mnt echo "127.0.0.1 localhost" >> /mnt/etc/hosts
-arch-chroot /mnt echo "::1 localhost" >> /mnt/etc/hosts
-arch-chroot /mnt echo "127.0.1.1 arch-laptop.localdomain arch-laptop" >> /mnt/etc/hosts
 
 # making sudoers do sudo stuff without requiring password typing
 arch-chroot /mnt sed -ie 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
--- pre-install.sh
+++ pre-install.sh
@@ -1,15 +1,15 @@
 echo "";
 echo "";
-echo "  /$$$$$$              /$$                      /$$$$$$                      /$$";      
-echo " /$$__  $$            | $$                     /$$__  $$                    | $$";      
-echo "| $$  \ $$ /$$   /$$ /$$$$$$    /$$$$$$       | $$  \ $$  /$$$$$$   /$$$$$$$| $$$$$$$"; 
-echo "| $$$$$$$$| $$  | $$|_  $$_/   /$$__  $$      | $$$$$$$$ /$$__  $$ /$$_____/| $$__  $$";
-echo "| $$__  $$| $$  | $$  | $$    | $$  \ $$      | $$__  $$| $$  \__/| $$      | $$  \ $$";
-echo "| $$  | $$| $$  | $$  | $$ /$$| $$  | $$      | $$  | $$| $$      | $$      | $$  | $$";
-echo "| $$  | $$|  $$$$$$/  |  $$$$/|  $$$$$$/      | $$  | $$| $$      |  $$$$$$$| $$  | $$";
-echo "|__/  |__/ \______/    \___/   \______/       |__/  |__/|__/       \_______/|__/  |__/";
-echo "";                                                                                      
-echo "     						Archlinux + i3 install script ";
+echo "  /$$$$$$              /$$                          /$$$$$$                      /$$";      
+echo " /$$__  $$            | $$                  /$$    /$$__  $$                    | $$";      
+echo "| $$  \ $$ /$$   /$$ /$$$$$$    /$$$$$$    | $$   | $$  \ $$  /$$$$$$   /$$$$$$$| $$$$$$$"; 
+echo "| $$$$$$$$| $$  | $$|_  $$_/   /$$__  $$ /$$$$$$$$| $$$$$$$$ /$$__  $$ /$$_____/| $$__  $$";
+echo "| $$__  $$| $$  | $$  | $$    | $$  \ $$|__  $$__/| $$__  $$| $$  \__/| $$      | $$  \ $$";
+echo "| $$  | $$| $$  | $$  | $$ /$$| $$  | $$   | $$   | $$  | $$| $$      | $$      | $$  | $$";
+echo "| $$  | $$|  $$$$$$/  |  $$$$/|  $$$$$$/   |__/   | $$  | $$| $$      |  $$$$$$$| $$  | $$";
+echo "|__/  |__/ \______/    \___/   \______/           |__/  |__/|__/       \_______/|__/  |__/";
+echo "";                                                                                     
+echo "     						  Archlinux + i3 install script ";
 echo "";                                                                                     
                                                                                       
 # disk
@@ -18,8 +18,29 @@ selected_disk=sda
 # root partition size, in GB
 root_partition_size=45
 
+# pacstrap packages
+BASE="base base-devel grub networkmanager linux-zen linux-headers linux-firmware xorg-server xorg-xinit xdg-user-dirs xorg-xrandr fzf intel-ucode cpupower"
+DRIVERS="xf86-video-ati xf86-video-amdgpu mesa libva-mesa-driver mesa-vdpau xf86-input-synaptics"
+INTERNET="firefox curl wget netctl wpa supplicant openssh transmission-gtk transmission-qt telegram-desktop"
+MULTIMEDIA="imagemagick vlc ffmpeg pulseaudio pamixer feh playerctl"
+UTILITIES="ntfs-3g exfat-utils zip unzip unrar p7zip gvfs gvfs-mtp tree man rofi alacritty hwloc xdotool dunst hsetroot picom dialog"
+DOCUMENTS_AND_TEXT="libreoffice-fresh vim xclip ranger"
+FONTS="ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts noto-fonts-emoji ttf-roboto ttf-cascadia-code"
+THEMES="papirus-icon-theme"
+SECURITY="rsync gnupg"
+SCIENCE=""
+OTHERS="tmux"
+DEVELOPER="code python git"
+CUSTOM=""
+
 # hostname
 hostname=arch-dell
+
+echo ""
+echo -e "\e[1;42m>>> START INSTALLATION...\e[0m"
+echo ""
+
+sleep 5
 
 # syncing system datetime
 timedatectl set-ntp true
@@ -77,18 +98,9 @@ sleep 5
 
 # Driver para Ati radeon y kernel linux-zen
 # pacstrap-ping desired disk
-pacstrap /mnt base base-devel vim grub networkmanager rofi feh linux-zen linux-headers \
-ntfs-3g alacritty git zsh intel-ucode cpupower xf86-video-ati xf86-video-amdgpu vlc \
-xorg-server xorg-xinit ttf-dejavu ttf-liberation ttf-inconsolata noto-fonts \
-firefox code zip unzip unrar gvfs gvfs-mtp xdg-user-dirs \
-pulseaudio pamixer telegram-desktop python python-pip wget \
-openssh xorg-xrandr noto-fonts-emoji imagemagick xclip light ranger \
-ttf-roboto playerctl papirus-icon-theme hwloc p7zip picom hsetroot \
-nemo linux-firmware tree man ttf-cascadia-code fzf \
-mesa libva-mesa-driver netctl wpa_supplicant dialog xf86-input-synaptics \
-mesa-vdpau zsh-syntax-highlighting xdotool cronie dunst entr \
-xf86-video-vmware python-dbus
-
+pacstrap /mnt ${BASE} ${DRIVERS} ${INTERNET} ${MULTIMEDIA} ${UTILITIES} \
+#${DOCUMENTS AND TEXT} ${FONTS} ${THEMES} ${SECURITY} ${SCIENCE} ${OTHERS} ${DEVELOPER}
+ 
 echo ""
 echo -e "\e[1;42m>>> PACSTRAP INSTALLATION COMPLETE...\e[0m"
 echo ""
@@ -99,37 +111,40 @@ sleep 5
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # enter base system
-arch-chroot /mnt
+#arch-chroot /mnt
 
 # setting machine name
 arch-chroot /mnt echo ${hostname} >> /mnt/etc/hostname
 
 # setting right timezone
-ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 
 # synchronizing timer
-hwclock --systohc
+arch-chroot /mnt hwclock --systohc
 
 # localizing system
-sed -ie 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/g' /etc/locale.gen
-sed -ie 's/#es_ES ISO-8859-1/es_ES ISO-8859-1/g' /etc/locale.gen
-sed -ie 's/#es_ES@euro ISO-8859-15/es_ES@euro ISO-8859-15/g' /etc/locale.gen
+arch-chroot /mnt sed -ie 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/g' /etc/locale.gen
+arch-chroot /mnt sed -ie 's/#es_ES ISO-8859-1/es_ES ISO-8859-1/g' /etc/locale.gen
+arch-chroot /mnt sed -ie 's/#es_ES@euro ISO-8859-15/es_ES@euro ISO-8859-15/g' /etc/locale.gen
 
 # generating locale
-locale-gen
+arch-chroot /mnt locale-gen
 
 # setting system language
-echo "LANG=es_ES.UTF-8" >> /mnt/etc/locale.conf
+arch-chroot /mnt echo "LANG=es_ES.UTF-8" >> /mnt/etc/locale.conf
+
+# hacer persistente lenguaje consola
+arch-chroot /mnt echo KEYMAP=es > /etc/vconsole.conf
 
 # setting hosts file
-echo "127.0.0.1 localhost" >> /mnt/etc/hosts
-echo "::1 localhost" >> /mnt/etc/hosts
-echo "127.0.1.1 arch-laptop.localdomain arch-laptop" >> /mnt/etc/hosts
+arch-chroot /mnt echo "127.0.0.1 localhost" >> /mnt/etc/hosts
+arch-chroot /mnt echo "::1 localhost" >> /mnt/etc/hosts
+arch-chroot /mnt echo "127.0.1.1 arch-laptop.localdomain arch-laptop" >> /mnt/etc/hosts
 
 # enabling font presets for better font rendering
-ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
-ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+arch-chroot /mnt ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+arch-chroot /mnt ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+arch-chroot /mnt ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
 echo ""
 echo -e "\e[1;42m>>> BASE SYSTEM SETTINGS COMPLETE...\e[0m"
